ba9808e6ca5341ba4f0359d4c1dfa26979146408
오태현 <jeffrey@kai-i.com>
2022-06-10 16:00:34 +0900

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# stateful_widget
+
+인프런 플러터 강의
+ - StatefulWidget
+
+ - [ ] StatefulWidget 선언하기
+ - [ ] Life Cycle 확인하기
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:stateful_widget/screen/home/HomeScreen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomeScreen(),
+  ));
+}
